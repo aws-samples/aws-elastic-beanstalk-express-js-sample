3d8efb52122d137380fc60ba8b960c4fe4cf9d9d
Maideen Asbak <asbakmaideen@gmail.com>
2020-12-20 18:20:51 +0530

    Update on top of pipeline test success

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Asbak,Your Full pipeline test succeeded!! Congtarz!! ));
+app.get('/', (req, res) => res.send('Asbak,Your Full pipeline test succeeded!! Congtarz man!! ));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
