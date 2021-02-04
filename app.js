de51a60938172dce605a848b7247563905beaef8
venkatakarthik94 <venkatakarthik94@gmail.com>
2021-02-04 10:12:45 +0530

    Message changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('App is running on host'));
+app.get('/', (req, res) => res.send('App is running on host and changed'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
