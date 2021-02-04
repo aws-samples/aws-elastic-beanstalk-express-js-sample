de449315d5a506fc1beaf6d8179520ccd6ec28e9
venkatakarthik94 <venkatakarthik94@gmail.com>
2021-02-04 10:23:50 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('App is running on host and changed'));
+app.get('/', (req, res) => res.send('App is running on host and changed with full pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
