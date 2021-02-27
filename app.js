6765052c1fda24b739f0439f7ea830eb4e8c250d
jakob brockner <jb@cdm.dk>
2021-02-27 10:14:48 +0100

    another change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hallo AWS!'));
+app.get('/', (req, res) => res.send('Hallo AWS - Now Changed!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
