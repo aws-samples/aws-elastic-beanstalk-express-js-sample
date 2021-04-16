1541bfe7602751ea7f1ff32d557b8e66e0c8341f
Panayiotis <panayiotis@investotech.com>
2021-04-16 10:30:39 +0300

    app.je change

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("AWS CodePipeline Demo"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
