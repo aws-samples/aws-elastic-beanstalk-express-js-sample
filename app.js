73fd0b15b06ea770654c86c4cbefbddffd044456
ZeynepOzge <53564453+ZeynepOzge@users.noreply.github.com>
2021-06-10 22:58:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Yba!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+git add app.js
+git commit -m "change message"
+git push
