967bc2c0cd8399b36aa4cff1a7978457cb89882e
HuskerGir1 <87146424+HuskerGir1@users.noreply.github.com>
2021-07-12 12:02:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Helloooooooo World!'));
+app.get('/', (req, res) => res.send('Jus playin wich ya!''));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
