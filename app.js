c87e593ed9d3f7dfd0059de2ac83a713c8bc81ad
Cloud User <ec2-user@ip-10-0-0-43.ap-south-1.compute.internal>
2021-07-28 06:14:31 +0000

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8081;
 
-app.get('/', (req, res) => res.send('App is running on host and changed with full pipeline'));
+app.get('/', (req, res) => res.send('App is running on host'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
