a8c5700a8bf77a985ea7b5f0a27438ce0950b3b7
Maclia <alltestx@gmail.com>
2021-09-24 14:33:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send("Hello World!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
