c9f3c05eb3af93442446c9d0a97fd91182254022
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-12-28 11:09:25 +0100

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8082;
+const port = 8080;
 
 app.get("/", (req, res) => res.send(`Hello World, i'm a student of aws...`));
 
