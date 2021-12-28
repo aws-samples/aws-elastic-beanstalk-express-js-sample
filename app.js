21b39ce125148dc44ec313060fe755d422181799
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2021-12-28 09:18:53 +0100

    change message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
+const port = 8082;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send(`Hello World, i'm a student of aws...`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
