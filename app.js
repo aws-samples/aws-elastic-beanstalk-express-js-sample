7500b7f34cdd483f90719c5ed519969ded7b6149
nicolasb <buecher.nicolas@gmail.com>
2022-01-19 22:38:25 +0100

    feat: Add message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('This is remembering me my newbie tutorials!'));
+app.get('/', (req, res) => res.send('If the PR have been manually aproved, you should read this!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
