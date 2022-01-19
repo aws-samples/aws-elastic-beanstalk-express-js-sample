e09b8b9112af55f64ea98d311e6dd73bd299ecce
nicolasb <buecher.nicolas@gmail.com>
2022-01-19 20:50:02 +0100

    feat: Change hello message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is remembering me my newbie tutorials!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
