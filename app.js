a3ff29c3039151d9bd82afebea5f5c6bec2383fb
Rahulsharma040488 <98212064+Rahulsharma040488@users.noreply.github.com>
2022-01-22 16:20:58 +0530

    Update app.js
    I am new to Git & GitHub and want to explore and learn to code.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World I am New to Git & GitHub and want tolearn how to use it!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
