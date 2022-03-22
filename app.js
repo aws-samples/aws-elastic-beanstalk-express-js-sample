629d8ee1f1eb2d840d832114187e32ead84515c8
Rafael Ortega Cohen <rafael.ortega@kikilogistics.co>
2022-03-21 21:30:33 -0500

    Test 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lunes'));
+app.get('/', (req, res) => res.send('Lunes Martes Miercoles'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
