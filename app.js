96499a16991c70e7344d374a6dc58edaea9d1f7d
yamaday0u <35hallelujah00@gmail.com>
2023-01-09 16:08:21 +0900

    demo test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Continues Delivery Test!'));
+app.get('/', (req, res) => res.send('Continues Delivery Demo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
