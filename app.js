4514dd8c893b3592e4286c85bbaa9ffc9821e427
Billa <mbilla@uab.edu>
2023-01-19 16:27:21 -0600

    change message

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+<<<<<<< HEAD
 app.get('/', (req, res) => res.send('Hello There Sample !'));
+=======
+app.get('/', (req, res) => res.send('Hello there This is a sample'));
+>>>>>>> 9e48ec6 (change message)
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
