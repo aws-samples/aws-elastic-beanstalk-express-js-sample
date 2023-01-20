b60a17688f0aa4729dc5955dd73d1518297ee072
Billa <mbilla@uab.edu>
2023-01-19 18:12:59 -0600

    new change

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-<<<<<<< HEAD
-app.get('/', (req, res) => res.send('Hello There Sample !'));
-=======
-app.get('/', (req, res) => res.send('Hello there This is a sample'));
->>>>>>> 9e48ec6 (change message)
+
+app.get('/', (req, res) => res.send('Hello There new Sample !'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
