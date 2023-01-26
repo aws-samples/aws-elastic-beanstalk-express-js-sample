655a6ede9f1dec93a3c7e9fa81bd89b42690c0d1
yamaday0u <35hallelujah00@gmail.com>
2023-01-26 23:58:32 +0900

    update from development

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Continues Delivery Demo Demo'));
+app.get('/', (req, res) => res.send('Continues Delivery From Development branch'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
