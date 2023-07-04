1c254b470107f449f9ceff7b578457749202f7bf
Jyothisha Muppur <jyothishamuppur@Jyothishas-MacBook-Pro.local>
2023-07-04 11:10:35 -0500

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey, This is my first hands on with AWS CI/CD'));
+app.get('/', (req, res) => res.send('I love SIMBA'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
