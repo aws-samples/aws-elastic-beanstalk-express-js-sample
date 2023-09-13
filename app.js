d993d61d5bfa52933a0f249c5e23e9517a191db8
Rocco2Fresh <144853676+Rocco2Fresh@users.noreply.github.com>
2023-09-12 19:38:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('I changed it'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+git add app.js
+git commit -m "Full pipeline test"
