d2f4381391e8c104d28ec1f08a9111d0290b107f
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-12-02 22:52:54 -0800

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('I will be great at AWS Cloud'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
