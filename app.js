cc17a736a43549ae6102ce05dd82cd1ff72a7c20
Olaf <97176207+olafjanssens@users.noreply.github.com>
2023-12-27 14:18:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ app.get('/', (req, res) => res.send('This is my first wudevva, being source mana
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+console.log(`crapperdapper!`);
