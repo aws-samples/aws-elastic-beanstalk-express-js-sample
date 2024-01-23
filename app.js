76be8b6bed7bd5e394f8b1761f482467b54b5fd8
moniarenata <157136227+moniarenata@users.noreply.github.com>
2024-01-23 12:44:51 +0000

    Update app.js
    git push

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('nice day'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+git add app.js
+git commit -m "change message"
