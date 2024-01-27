45af123bc74334548f8cfe89fb3a426bd731cbb9
Mkrtich <52213410+Mkrtich87@users.noreply.github.com>
2024-01-27 13:49:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('You have power over your mind — not outside events!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
