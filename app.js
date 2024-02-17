b9cdb012e5e113617f7675696de7d7eee4670af1
bence <borbelybence98@gmail.com>
2024-02-17 11:06:15 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Here we go again'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
