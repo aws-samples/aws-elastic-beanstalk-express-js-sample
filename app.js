8d177a4edba6a14b18ecacd123512f7b69e23554
Elshan F. Ahmedov <ahmedovelshan@gmail.com>
2024-02-25 17:15:06 +0400

    last test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test successfull'));
+app.get('/', (req, res) => res.send('Last chnage and test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
