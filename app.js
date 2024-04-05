71ee9f330d43ec99d8d7a0593481cb97465dab4d
Manohar <manoharraog@gmail.com>
2024-04-05 16:29:27 -0400

    added review stage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('welcome to k21acadamey projects 4-5-2024!'));
+app.get('/', (req, res) => res.send('I have added a review stage in pipeline with manual approval condition!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
