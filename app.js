adae4738d76aa0e6f79c3bdc7de25f44e04dd1cc
Peter Griekspoor <peterg@checkpoint.com>
2024-06-25 10:16:03 +0300

    change message

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - this is update 1.1!'));
-
+app.get('/', (req, res) => res.send('Hello World - this is update 3.1!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
