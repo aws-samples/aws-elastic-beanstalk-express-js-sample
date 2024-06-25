426f36bea2bb9de26d9677e8dc30b7fc9ceff68a
Peter Griekspoor <peterg@checkpoint.com>
2024-06-25 16:22:34 +0300

    new line of code

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - this version is scanned again!'));
+app.get('/', (req, res) => res.send('Hello World - this version is scanned again and again!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
