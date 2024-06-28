2955a50c81353840b8ba93dc6f98f7a50747ff16
Peter Griekspoor <peterg@checkpoint.com>
2024-06-28 17:29:11 +0300

    asdf

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - working on Friday - autsj!!'));
+app.get('/', (req, res) => res.send('Hello World - working on Friday - au!!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
