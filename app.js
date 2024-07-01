37021c1020b0c64cb0aa5416302f89b3aedd50aa
Peter Griekspoor <peterg@checkpoint.com>
2024-07-01 11:13:19 +0300

    changed the policy using chatgpt

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to our training class on Firewalls R80 again'));
+app.get('/', (req, res) => res.send('Welcome to our training class on Firewalls R80 again :)'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
