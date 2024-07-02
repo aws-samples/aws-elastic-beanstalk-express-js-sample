7f1b44545d2e8c02eaee2096b00afb4a6eb30e5d
getin2cloudnow <getin2cloud@gmail.com>
2024-07-02 07:47:39 +0300

    made a mistake

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to our new training class on Firewalls R80 :)'));
+app.get('/', (req, res) => res.send('Welcome to our new training class on Code Security :)'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
