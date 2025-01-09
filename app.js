6d91223d1fcc8dd824c9ae3aed565e32d5c5957d
Emilis01 <emilisdov@yahoo.com>
2025-01-09 14:42:23 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Pagaliau kazkas pavyko 1 !'));
+app.get('/', (req, res) => res.send('Hello World from Pagaliau kazkas pavyko 2 !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
