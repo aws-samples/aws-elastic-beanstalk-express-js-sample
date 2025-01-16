331df239d5bf6108c12fd3fefbe3c7fc4d938ce4
Emilis01 <emilisdov@yahoo.com>
2025-01-16 19:51:38 +0200

    some changes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Pagaliau kazkas pavyko 5 !'));
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World from PagaliauSSS kazkas pavyko 5 !'));
+app.get('/user', (req, res) => res.send({
+	name: "petras",
+	email: "petras@gmail.com",
+	password: "123456",
+}));
+app.post('/user', (req, res) => {
+	const users = req.body;
+
+	console.log('users req.body', req.body);
+
+	res.send({
+		message: 'New user was added to the list',
+	});
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
