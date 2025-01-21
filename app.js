ce585ed475a5796e627d72d56ede6be810eb07c9
Emilis01 <emilisdov@yahoo.com>
2025-01-21 17:18:56 +0200

    some changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 8080;
 
 app.use(cors());
 // KOMENTARASss
+// asdasdasdasdasd
 app.get('/', (req, res) => res.send(`Hello World from PagaliauSSS kazkas pavyko! Versija 7`));
 app.get('/user', (req, res) => res.send({
 	name: "petras",
