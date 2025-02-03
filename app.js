699a2c09a38cb9b19629b28a32eb124ef2833582
farat123 <4485farat@gmail.com>
2025-02-03 12:48:08 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Farat Bagawan!'));
+app.get('/', (req, res) => res.send('Hello Farat Bagawan!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
