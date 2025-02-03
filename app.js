784bd2bd5f2d28a4303be599071005893957a273
farat123 <54427563+farat123@users.noreply.github.com>
2025-02-03 15:25:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Farat Bagawan!!'));
+app.get('/', (req, res) => res.send('Hello Farat syed'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
