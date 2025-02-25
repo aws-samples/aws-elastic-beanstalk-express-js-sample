11b4ec04ea8f2c50bb2f1fb3afd5f282590985c9
david <xvr2010@gmail.com>
2025-02-25 07:46:33 +0200

    add guys massage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World11252421!'));
+app.get('/', (req, res) => res.send('Hello Guys!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
