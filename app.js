4deed3fb80e331f8250cf1beaa0f0c92ac51f448
JSnell <113995138+JSdotSERC@users.noreply.github.com>
2025-02-26 14:03:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('WHERE THE ACTUAL FUCK IS CHARLES LEE?! FOR REAL!!'));
+app.get('/', (req, res) => res.send('WHERE IS CHARLES LEE?! FOR REAL!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
