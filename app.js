4d952f0d2b674ec193e18adccfd6f90c73e9f010
Sudheerreddy43 <kndisudheerreddy007@gmail.com>
2025-04-17 15:06:09 -0400

    Finl push with congras message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World We are ananomous!'));
+app.get('/', (req, res) => res.send('Hollaaaa Successfully completed the project!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
